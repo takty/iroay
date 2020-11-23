1642d583b80ea8979ba43529e30912143fe6187d
Takuto Yanagida <yanagida@stxst.com>
2020-11-24 00:12:32 +0900

    Replace gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('compile', () => {
 	return gulp.src(['src/**/[^_]*.js'])
 		.pipe($.plumber())
 		.pipe($.include())
-		.pipe($.deleteLines({ filters: [/\/\/=/] }))
+		.pipe($.rmLines({ filters: [/\/\/=/] }))
 		.pipe($.replace(/^\t$/gm, ''))
 		.pipe($.sourcemaps.init())
 		.pipe($.terser())
