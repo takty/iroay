7ce98ba9857b2308c60509625c88db1ed7243559
Takuto Yanagida <yanagida@stxst.com>
2022-08-13 23:54:12 +0900

    Improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
+/**
+ * Gulpfile
+ *
+ * @author Takuto Yanagida
+ * @version 2022-08-13
+ */
+
+'use strict';
+
 const gulp = require('gulp');
 const $    = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
 
-gulp.task('compile', () => {
-	return gulp.src(['src/**/[^_]*.js'])
-		.pipe($.plumber())
-		.pipe($.include())
-		.pipe($.rmLines({ filters: [/\/\/=/] }))
-		.pipe($.replace(/^\t$/gm, ''))
-		.pipe($.sourcemaps.init())
-		.pipe($.terser())
-		.pipe($.rename({ extname: '.min.js' }))
-		.pipe($.sourcemaps.write('./map'))
-		.pipe(gulp.dest('dist'));
-});
+const compile = () => gulp.src(['src/**/[^_]*.js'])
+	.pipe($.plumber())
+	.pipe($.include())
+	.pipe($.rmLines({ filters: [/\/\/=/] }))
+	.pipe($.replace(/^\t$/gm, ''))
+	.pipe($.sourcemaps.init())
+	.pipe($.terser())
+	.pipe($.rename({ extname: '.min.js' }))
+	.pipe($.sourcemaps.write('./map'))
+	.pipe(gulp.dest('dist'));
 
-gulp.task('watch', () => {
-	gulp.watch('src/**/*.js', gulp.series('compile'));
-});
+const watch = done => {
+	gulp.watch('src/**/*.js', gulp.series(compile));
+	done();
+};
 
-gulp.task('default', gulp.series('compile', 'watch'));
+exports.default = gulp.series(compile, watch);
