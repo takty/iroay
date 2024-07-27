de6d8910565b011165930cac58ebe54814e7bed7
Takuto Yanagida <tokutadaginaya@gmail.com>
2024-07-27 18:08:58 +0900

    Refactoring

--- src/colorjst.ts
+++ src/colorjst.ts
@@ -8,7 +8,7 @@ export { YIQ } from './_cs-yiq';
 export { Yxy } from './_cs-yxy';
 export { Munsell } from './_cs-munsell';
 export { PCCS } from './_cs-pccs';
-export { convert, isRGBSaturated, isYxySaturated, isMunsellSaturated } from './_cs-function';
+export { convert, getConverter, isRGBSaturated, isYxySaturated, isMunsellSaturated } from './_util';
 
 export { Evaluation } from './_eval';
 export { AgeSimulation } from './_sim-age';
