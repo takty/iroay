d70835d84ce0582cb32bfb46bda43afc4feac6ab
Takuto Yanagida <tokutadaginaya@gmail.com>
2026-06-16 08:04:13 +0900

    FIx configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		include: ['src/_tests/**/*.test.ts'],
-		globals: true,
+		globals: false,
 		environment: 'node',
 		coverage: {
 			provider: 'v8',
